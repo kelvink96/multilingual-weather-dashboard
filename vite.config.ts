0befd6eb485e824946af05c9a449cf485e812116
Kelvin Kiptum Kiprop <kelvin.kiprop96@gmail.com>
2024-04-29 16:18:16 +0300

    fix: reset branch

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-    base: '/multilingual-weather-dashboard/',
+    base: '/',
     plugins: [react()],
     test: {
         globals: true,
